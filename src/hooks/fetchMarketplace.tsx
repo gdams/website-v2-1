41a9a22171fe1c358a100b7cdc61c7cbbc7f117b
George Adams <georgeadams1995@gmail.com>
2022-05-24 12:21:44 +0100

    DO NOT MERGE - Switch to staging so we can check things work

--- src/hooks/fetchMarketplace.tsx
+++ src/hooks/fetchMarketplace.tsx
@@ -1,4 +1,4 @@
-const baseUrl = 'https://marketplace-api.adoptium.net';
+const baseUrl = 'https://staging-marketplace-api.adoptium.net';
     
 export async function getAllPkgsForVersion(
         version: number,
