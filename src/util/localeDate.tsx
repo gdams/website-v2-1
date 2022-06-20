5fabb006c2c51f3bce92f2210f41a3c98940f733
George Adams <georgeadams1995@gmail.com>
2022-06-20 12:26:11 +0100

    Update src/util/localeDate.tsx
    Co-authored-by: Martijn Verburg <martijnverburg@gmail.com>

--- src/util/localeDate.tsx
+++ src/util/localeDate.tsx
@@ -1,5 +1,5 @@
 export function localeDate (date: string, language: string) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const d = new Date(date);
+    const localeDate = new Date(date);
     return d.toLocaleDateString(language, options)
 }
