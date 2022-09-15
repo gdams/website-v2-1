0c17efef8f4f837a5e646a2434fd81acc43895a9
George Adams <georgeadams1995@gmail.com>
2022-09-15 12:04:06 +0100

    use react-table-bootstrap

--- src/templates/releaseNoteTemplate.tsx
+++ src/templates/releaseNoteTemplate.tsx
@@ -1,12 +1,35 @@
 import { graphql } from 'gatsby'
 import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 
+const { SearchBar } = Search;
+
 export default function Template ({ data, pageContext }) {
   const { allReleaseNotes } = data // data.allReleaseNotes holds our data
   const releaseNotes = allReleaseNotes.edges
+
+  const columns = [{
+    dataField: 'node.link',
+    text: 'Issue',
+    formatter: (cell, row) => (
+      <a href={row.node.link}>{row.node.id}</a>
+    ),
+  }, {
+    dataField: 'node.component[0].name',
+    text: 'Component'
+  }, {
+    dataField: 'node.priority',
+    text: 'Priority'
+  }, {
+    dataField: 'node.title',
+    text: 'Title'
+  }];
+
   return (
     <Layout>
       <Seo title={`Release Notes - ${pageContext.version}`} />
@@ -15,34 +38,23 @@ export default function Template ({ data, pageContext }) {
           <div className='col-lg-8 col-md-8 mx-auto'>
           <h1 className='fw-light'>{`Release Notes - ${pageContext.version}`}</h1>
         </div>
-        <div className="p-5 text-center">
-          <table className='table text-start table-sm table-striped align-middle table-hover'>
-            <thead className='table-dark'>
-              <tr>
-                <th scope='col'>Issue</th>
-                <th scope='col'>Component</th>
-                <th scope='col'>Priority</th>
-                <th scope='col'>Title</th>
-              </tr>
-            </thead>
-            <tbody>
-              {releaseNotes.map(
-                (issue, i): string | JSX.Element =>
-                  issue && (
-                    <tr>
-                      <td><a target='_blank' rel='noopener noreferrer' href={issue.node.link}>{issue.node.id}</a></td>
-                      {
-                        issue.node.component.length ? issue.node.component.map((component) =>
-                        (<td> {component.name} </td>)) : '/'
-                      }
-                      <td>{issue.node.priority}</td>
-                      <td>{issue.node.title}</td>
-                    </tr>
-                  )
-                )}
-            </tbody>
-          </table>
-        </div>
+        <ToolkitProvider
+          keyField='node.id'
+          data={ releaseNotes }
+          columns={ columns }
+          search
+        >
+          {
+            props => (
+              <>
+                <SearchBar { ...props.searchProps } />
+                <div className="p-5">
+                  <BootstrapTable striped hover bodyClasses="nowrap" pagination={ paginationFactory() } { ...props.baseProps } />
+                </div>
+              </>
+            )
+          }
+        </ToolkitProvider>
       </div>
     </section>
     </Layout>
