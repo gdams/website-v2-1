a6f6bffa1042843f703dbc9147d6a2c369e5c2ff
George Adams <georgeadams1995@gmail.com>
2022-09-16 11:48:33 +0100

    initial release notes page prototype
    Co-authored-by: Beth Griggs <bethanyngriggs@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
-const createMultilingualRedirects = require('./i18n-redirects')
+const axios = require('axios');
+const crypto = require('crypto');
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+const createMultilingualRedirects = require('./i18n-redirects');
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -41,6 +43,29 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     })
   })
+
+  const releaseNoteTemplate = path.resolve('./src/templates/releaseNotesTemplate.tsx')
+
+  const releaseNoteResults = await graphql(`
+    {
+      allReleaseNotes {
+        group(field: version) {
+          distinct(field: version)
+        }
+      }
+    }
+  `)
+
+  releaseNoteResults.data.allReleaseNotes.group.forEach(({ distinct }) => {
+    // Create page for each release note version
+    createPage({
+      path: `/release-notes/${distinct.toString()}`,
+      component: releaseNoteTemplate,
+      context: {
+        version: distinct.toString()
+      }
+    })
+  })
 }
 
 exports.onCreateNode = async ({ node, actions, getNode, loadNodeContent }) => {
@@ -54,4 +79,54 @@ exports.onCreateNode = async ({ node, actions, getNode, loadNodeContent }) => {
       value
     })
   }
+}
+
+exports.sourceNodes = async ({ actions }) => {
+  const { createNode } = actions;
+  // List of versions to get release notes for
+  // TODO: Change to dynamically get versions
+  const SAMPLE_VERSIONS = ['openjdk8u342', '11.0.16', '17.0.4', '18.0.2']
+  const OPENJDK_API = 'https://bugs.openjdk.org/rest/api/latest';
+  for (let version of SAMPLE_VERSIONS) {
+    const url = `${OPENJDK_API}/search?jql=project=JDK AND fixVersion=${version}&maxResults=1000`;
+    axios.get(url).then(res => {
+      // map into these results and create nodes
+      res.data.issues.map((issue) => {
+        // Create your node object
+        const releaseNoteNode = {
+          // Required fields
+          id: issue.key,
+          parent: `__SOURCE__`,
+          internal: {
+            type: `ReleaseNotes`, // name of the graphQL query --> allReleaseNotes {}
+            // contentDigest will be added just after
+            // but it is required
+          },
+          children: [],
+          version: issue.fields.fixVersions[0].name.replace('openjdk', ''), // remove openjdk prefix from 8u
+
+          // Other fields that you want to query with graphQl
+          title: issue.fields.summary,
+          priority: issue.fields.priority.id,
+          component: issue.fields.components,
+          subcomponent: `${issue.fields.components[0].name}${issue.fields.customfield_10008?.name ? "/" + issue.fields.customfield_10008?.name : ''}`,
+          link: `https://bugs.openjdk.org/browse/${issue.key}`,
+          // etc... 
+        }
+
+        // Get content digest of node. (Required field)
+        const contentDigest = crypto
+          .createHash(`md5`)
+          .update(JSON.stringify(releaseNoteNode))
+          .digest(`hex`);
+        // add it to userNode
+        releaseNoteNode.internal.contentDigest = contentDigest;
+
+        // Create node with the gatsby createNode() API
+        createNode(releaseNoteNode);
+      });
+    }
+  )};
+
+  return;
 }
---
+++ src/templates/releaseNotesTemplate.tsx
@@ -0,0 +1,90 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
+
+const { SearchBar } = Search;
+
+export default function Template ({ data, pageContext }) {
+  const { allReleaseNotes } = data // data.allReleaseNotes holds our data
+  const releaseNotes = allReleaseNotes.edges
+
+  const columns = [{
+    dataField: 'node.link',
+    text: 'Issue',
+    formatter: (cell, row) => (
+      <a href={row.node.link}>{row.node.id}</a>
+    ),
+    sort: true
+  }, {
+    dataField: 'node.subcomponent',
+    text: 'Component',
+    sort: true
+  }, {
+    dataField: 'node.priority',
+    text: 'Priority',
+    sort: true
+  }, {
+    dataField: 'node.title',
+    text: 'Title'
+  }];
+
+  return (
+    <Layout>
+      <Seo title={`Release Notes - ${pageContext.version}`} />
+      <section className='py-5 container-xxl'>
+        <h1 className='fw-light text-center'>{`Release Notes - ${pageContext.version}`}</h1>
+        
+        <ToolkitProvider
+          keyField='node.id'
+          data={ releaseNotes }
+          columns={ columns }
+          search
+        >
+          {
+            props => (
+              <>
+                <div className="p-5">
+                  <SearchBar className="" { ...props.searchProps } />
+                  <BootstrapTable striped hover bodyClasses="text-nowrap" pagination={ paginationFactory() } { ...props.baseProps } />
+                </div>
+              </>
+            )
+          }
+        </ToolkitProvider>
+    </section>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($version: String!, $language: String!) {
+    allReleaseNotes(filter: {version: {eq: $version}}, sort: {fields: id}) {
+      edges {
+        node {
+          id
+          title
+          link
+          priority
+          component {
+            name
+          }
+          subcomponent
+        }
+      }
+    }
+    locales: allLocale(filter: {language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`
