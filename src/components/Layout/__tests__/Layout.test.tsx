6cfab62c3f24630bd634445e7cad4557b608aee4
George Adams <georgeadams1995@gmail.com>
2022-09-20 10:58:40 +0100

    mock Banner in layout component

--- src/components/Layout/__tests__/Layout.test.tsx
+++ src/components/Layout/__tests__/Layout.test.tsx
@@ -3,6 +3,13 @@ import { render } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest'
 import Layout from '..';
 
+// Mock the Banner component as this changes frequently
+vi.mock('../../Banner', () => {
+  return {
+    default: () => <div>mock-banner</div>,
+  };
+})
+
 describe('Layout component', () => {
   it('renders correctly with data', () => {
     const { container } = render(
